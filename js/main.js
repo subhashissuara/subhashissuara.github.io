61ae408931c7d69583a9b708c5b89e0fb16c1f5f
Subhashis Suara <subhashis.suara999@gmail.com>
2021-01-26 21:33:44 +0530

    Refactor main.js & Add Anime Heaven Project

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-/* 
+/*
 	Author: Subhashis Suara
 */
 
+// Global Variables
+const sections = document.querySelectorAll(".section");
 const navMenuBtn = document.querySelector(".menu-btn");
+const modeBtn = document.querySelector(".mode-btn");
 const navMenuDesktop = document.querySelector(".navbar-desktop");
 const navMenuMobile = document.querySelector(".navbar-mobile");
 const preloader = document.querySelector(".preloader-container");
@@ -14,7 +17,128 @@ navLinksList.forEach((navLink) => {
   hashIdList.push(navLink.hash);
 });
 
-// Effects
+/*
+  IIFEs
+*/
+
+// Detect Theme
+(() => {
+  if (localStorage.getItem("theme") !== null) {
+    if (localStorage.getItem("theme") === "dark") {
+      modeBtn.querySelector("i").classList.add("fa-sun");
+      document.body.classList.add("dark-mode");
+      document.querySelector(".bg").classList.remove("bg-effect-light");
+      document.querySelector(".bg").classList.add("bg-effect-dark");
+    } else {
+      modeBtn.querySelector("i").classList.add("fa-moon");
+      document.body.classList.remove("dark-mode");
+      document.querySelector(".bg").classList.add("bg-effect-light");
+      document.querySelector(".bg").classList.remove("bg-effect-dark");
+    }
+  }
+})();
+
+// Show Active Section
+(() => {
+  sections.forEach((section) => {
+    if (!section.classList.contains("active-section")) {
+      section.classList.add("hide-section");
+    }
+  });
+})();
+
+// OnLoad Events
+window.addEventListener("load", () => {
+  // Navigate from External Redirects
+  changePage();
+
+  // Preloader
+  preloader.classList.add("preloader-fade");
+  setInterval(() => preloader.classList.add("preloader-hide"), 1000);
+
+  // Dark Mode Icon
+  if (document.body.classList.contains("dark-mode")) {
+    modeBtn.querySelector("i").classList.add("fa-sun");
+  } else {
+    modeBtn.querySelector("i").classList.add("fa-moon");
+  }
+});
+
+// OnPopState Events
+window.addEventListener("popstate", () => {
+  // Navigate Back & Forth from Website URL History
+  changePage(false);
+});
+
+/*
+  Named Functions
+*/
+
+// Function to Change Active Section
+function changeActiveSection(hashId) {
+  document
+    .querySelector(".section.active-section")
+    .classList.add("hide-section");
+  document
+    .querySelector(".section.active-section")
+    .classList.remove("active-section");
+  document.querySelector(hashId).classList.add("active-section");
+  document.querySelector(hashId).classList.remove("hide-section");
+}
+
+// Function to Change Active Link
+function changeActiveNavLink(hashId) {
+  navMenuDesktop
+    .querySelector(".link-btn.nav-active")
+    .classList.remove("nav-active");
+  navMenuMobile
+    .querySelector(".link-btn.nav-active")
+    .classList.remove("nav-active");
+  navMenuDesktop
+    .querySelector(`a[href="${hashId}"]`)
+    .classList.add("nav-active");
+  navMenuMobile
+    .querySelector(`a[href="${hashId}"]`)
+    .classList.add("nav-active");
+}
+
+// Function to Change Page
+function changePage(resetContactForm = true) {
+  hashId = window.location.hash;
+  let IdValid = false;
+
+  // Check if navigation id is valid
+  for (let index = 0; index < hashIdList.length; index++) {
+    if (window.location.hash === hashIdList[index]) {
+      IdValid = true;
+    }
+  }
+
+  // Navigate to home if id is blank or invalid
+  if (!IdValid) {
+    if (hashId !== "") {
+      // Add home id to URL if id is invalid
+      window.location.hash = "#home";
+    }
+    IdValid = true;
+    hashId = "#home";
+  }
+
+  if (IdValid) {
+    // Change Active Section
+    changeActiveSection(hashId);
+
+    // Change Active Navigation Link
+    changeActiveNavLink(hashId);
+
+    // Reset Contact Form after FormSpree Redirect
+    if (hashId === "#contact" && resetContactForm) {
+      document.querySelector(".contact-form form").reset();
+    }
+  }
+}
+
+// Mobile Navigation FadeOut Function
 function menuFadeOutEffect() {
   navMenuMobile.classList.remove("nav-mobile-open");
   menuFadeOut.classList.add("effect-active");
@@ -23,52 +147,9 @@ function menuFadeOutEffect() {
   }, 300);
 }
 
-// Function to change page based on id
-function changePage(resetContactForm = true) {
-  let IdValid = false;
-  for (let index = 0; index < hashIdList.length; index++) {
-    if (window.location.hash === hashIdList[index]) {
-      IdValid = true;
-    }
-  }
-  if (IdValid) {
-    const hashId = window.location.hash;
-    // Section Changes
-    document
-      .querySelector(".section.active-section")
-      .classList.add("hide-section");
-    document
-      .querySelector(".section.active-section")
-      .classList.remove("active-section");
-    document.querySelector(hashId).classList.add("active-section");
-    document.querySelector(hashId).classList.remove("hide-section");
-
-    // Nav Link Changes
-    navMenuDesktop
-      .querySelector(".link-btn.nav-active")
-      .classList.remove("nav-active");
-    navMenuMobile
-      .querySelector(".link-btn.nav-active")
-      .classList.remove("nav-active");
-    navMenuDesktop
-      .querySelector(`a[href="${hashId}"]`)
-      .classList.add("nav-active");
-    navMenuMobile
-      .querySelector(`a[href="${hashId}"]`)
-      .classList.add("nav-active");
-
-    // Reset Contact Form after FormSpree redirect
-    if (hashId === "#contact" && resetContactForm) {
-      document.querySelector(".contact-form form").reset();
-    }
-  }
-}
-
-// Detect & navigate from id in URL if detected on first load
-document.querySelector("body").onload = () => changePage();
-
-// Detect & navigate from id in URL if detected from history
-window.onpopstate = () => changePage(false);
+/*
+  Website Navigation
+*/
 
 // Core Navigation
 document.addEventListener("click", (event) => {
@@ -77,30 +158,13 @@ document.addEventListener("click", (event) => {
     if (event.target.hash !== "") {
       event.preventDefault();
       const hashId = event.target.hash;
-      // Section Changes
-      document
-        .querySelector(".section.active-section")
-        .classList.add("hide-section");
-      document
-        .querySelector(".section.active-section")
-        .classList.remove("active-section");
-      document.querySelector(hashId).classList.add("active-section");
-      document.querySelector(hashId).classList.remove("hide-section");
 
-      // Nav Link Changes
+      // Change Active Section
+      changeActiveSection(hashId);
+
+      // Change Active Navigation Link
       if (!event.target.classList.contains("nav-active")) {
-        navMenuDesktop
-          .querySelector(".link-btn.nav-active")
-          .classList.remove("nav-active");
-        navMenuMobile
-          .querySelector(".link-btn.nav-active")
-          .classList.remove("nav-active");
-        navMenuDesktop
-          .querySelector(`a[href="${hashId}"]`)
-          .classList.add("nav-active");
-        navMenuMobile
-          .querySelector(`a[href="${hashId}"]`)
-          .classList.add("nav-active");
+        changeActiveNavLink(hashId);
       }
 
       // Mobile Navigation UI Close
@@ -123,19 +187,11 @@ document.addEventListener("click", (event) => {
   closeNavMenuBtn.addEventListener("click", menuFadeOutEffect);
 })();
 
-// Hide all sections except active class section
-(() => {
-  const sections = document.querySelectorAll(".section");
-  sections.forEach((section) => {
-    if (!section.classList.contains("active-section")) {
-      section.classList.add("hide-section");
-    }
-  });
-})();
+/*
+  Dark & Light Mode
+*/
 
-// Dark & Light Mode
-modeBtn = document.querySelector(".mode-btn");
-
+// Mode Button
 modeBtn.addEventListener("click", () => {
   modeBtn.querySelector("i").classList.toggle("fa-sun");
   modeBtn.querySelector("i").classList.toggle("fa-moon");
@@ -146,34 +202,5 @@ modeBtn.addEventListener("click", () => {
     localStorage.setItem("theme", "dark");
   } else {
     localStorage.setItem("theme", "light");
-  }
-});
-
-(function storedThemeLoad() {
-  if (localStorage.getItem("theme") !== null) {
-    if (localStorage.getItem("theme") === "dark") {
-      modeBtn.querySelector("i").classList.add("fa-sun");
-      document.body.classList.add("dark-mode");
-      document.querySelector(".bg").classList.remove("bg-effect-light");
-      document.querySelector(".bg").classList.add("bg-effect-dark");
-    } else {
-      modeBtn.querySelector("i").classList.add("fa-moon");
-      document.body.classList.remove("dark-mode");
-      document.querySelector(".bg").classList.add("bg-effect-light");
-      document.querySelector(".bg").classList.remove("bg-effect-dark");
-    }
-  }
-})();
-
-window.addEventListener("load", () => {
-  // Preloader
-  preloader.classList.add("preloader-fade");
-  setInterval(() => preloader.classList.add("preloader-hide"), 1000);
-
-  // Dark Mode Icon
-  if (document.body.classList.contains("dark-mode")) {
-    modeBtn.querySelector("i").classList.add("fa-sun");
-  } else {
-    modeBtn.querySelector("i").classList.add("fa-moon");
   }
 });
